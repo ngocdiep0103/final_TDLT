a42b734ed1922acbdf9449f9270dfb33a3f1daa6
thaoong <thao4985@gmail.com>
2021-12-09 18:07:28 +0700

    Update troly-khac.py

--- troly-khac.py
+++ troly-khac.py
@@ -1,5 +1,6 @@
 import pyttsx3
 import speech_recognition as sr
+from datetime import date, datetime
 
 def speak(text):
     troly=pyttsx3.init()
@@ -10,7 +11,7 @@ def speak(text):
 def get_audio():
     r = sr.Recognizer()
     with sr.Microphone() as source:
-        print("............")
+        print("Assitant: I am listening...")
         audio = r.listen(source, phrase_time_limit=5)
         try:
             text = r.recognize_google(audio)
@@ -31,29 +32,28 @@ def get_text():
 def welcome():
     hour = datetime.now().hour
     if hour >=6 and hour<12:
-        speak('Good morning')
+        speak('Good morning!')
     elif hour >=12 and hour<18:
-        speak('Good afternoon')
+        speak('Good afternoon!')
     else:
-        speak('Good night')
+        speak('Good night!')
 
 def get_day():
     today = date.today()
     # Hàm strftime() trong Python trả về một chuỗi biểu diễn giá trị ngày, giờ và thời gian bằng cách sử dụng các đối tượng date, time và datetime.
     d2 = today.strftime("%B %d, %Y")
-    print("Today is " + d2)
     return d2
 
 def get_time():
     now = datetime.now()
     time = now.strftime("%I:%M %p")
-    print("Current time is : " + time )
     return time
 
 def stop():
     speak("See you later!")
 
 def assistant():
+    welcome()
     speak('What is your name?')
     name = get_text()
     speak('Hello ' +str(name))
@@ -65,11 +65,15 @@ def assistant():
             stop()
             break
         elif 'time' in str(text):
-            get_time()
-        elif 'day' in str(text):
-            get_day()
+            time=get_time()
+            speak('It is '+str(time))
+        elif 'today' in str(text):
+            today=get_day()
+            speak('Today is '+str(today))
         elif 'stop' in str(text) or 'bye' in str(text):
             stop()
             break
+        else:
+            speak('I can not understand, please repeat!')
 
 assistant()
